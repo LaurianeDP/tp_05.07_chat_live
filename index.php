be7bfafa54b5f3de954bc52de9f8f4ba683b78a8
LaurianeDP <lauriane.pauleau.duran@gmail.com>
2022-07-05 16:19:50 +0200

    changement de index en php, ajout des pages de connexion à la bdd et du header html

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    include_once "connect.php";
+    include_once "header_html.php";
+?>
+
+<body>
+    <h1>Modifie quelque chose</h1>
+    <p>Une modification lauriane aussi</p>
+    <p>Une modification ici aussi</p>
+</body>
+</html>
