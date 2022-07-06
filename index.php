2c3a52d77b397accd812c4ef2458e8f75cba059a
LaurianeDP <lauriane.pauleau.duran@gmail.com>
2022-07-06 09:51:06 +0200

    création du header et de la portion de gauche

--- index.php
+++ index.php
@@ -2,15 +2,34 @@
     include_once "connect.php";
     include_once "header_html.php";
 
-    if(!isset($_SESSION["utilisateur"])) //Si l'utilisateur n'est pas connecté, renvoit à la page de connexion
-    {
-        header('Location: ./connexion.php');
-    }
+    // if(!isset($_SESSION["utilisateur"])) //Si l'utilisateur n'est pas connecté, renvoit à la page de connexion
+    // {
+    //     header('Location: ./connexion.php');
+    // }
 ?>
 
 <body>
-    <h1>Modifie quelque chose</h1>
-    <p>Une modification lauriane aussi</p>
-    <p>Une modification ici aussi</p>
+
+        <div class="container">
+            <!-- Le header avec le titre de la section de gauche le nom de l'utilisateur et le logo à droite -->
+            <div class="row bg-secondary p-3 d-flex align-items-center text-light">
+                <div class="col-3 d-flex justify-content-center">
+                    <h3>Récents</h3>
+                </div>
+                <div class="col-6 d-flex justify-content-start">
+                    <h3>Nom de l'utilisateur</h3>
+                </div>
+                <div class="col-2 d-flex justify-content-end">
+                    <p>Logo</p>
+                </div>
+            </div>
+            <!-- Fin du header au dessus -->
+            <div class="row">
+                <div class="col-3 bg-secondary text-light border-top border-dark p-2" id="contact-list">
+                    <!-- Du PHP pour afficher la liste d'amis et les conversation liées si nécessaire -->
+                </div>
+            </div>
+        </div>
+
 </body>
 </html>
