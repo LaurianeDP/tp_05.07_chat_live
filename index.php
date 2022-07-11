09f11b2641b3814254105c009552f295ffe4f4e2
LaurianeDP <lauriane.pauleau.duran@gmail.com>
2022-07-11 15:07:51 +0200

    fix affichage conversation

--- index.php
+++ index.php
@@ -93,24 +93,24 @@
                                 $requete=$connexion->prepare($sql_ami);
                                 $requete->execute();
                                 while ($ami= $requete->fetch()) {
-                                $ami_pseudo=$ami['pseudo'];
-                                $ami_id=$ami['id_ami'];
-                                $sql_conv2= "SELECT * FROM conversations WHERE (utilisateur_1=$utilisateur OR utilisateur_2=$utilisateur) AND (utilisateur_1=$ami_id OR utilisateur_2=$ami_id)";
-                                $requete_conv2=$connexion->prepare($sql_conv2);
-                                $requete_conv2->execute();
-                                $conv_result2=$requete_conv2->fetch();
-                                if(!$conv_result2) {
-                                    $id_conv="";
-                                    $last_message="Pas encore de message";
-                                }
-                                else {
-                                    $id_conv=$conv_result2['id_conversation'];
-                                    $sql_last_message= "SELECT contenu FROM messages WHERE id_conversation=$id_conv ORDER BY time_stamp DESC LIMIT 1";
-                                    $requete_last_message=$connexion->prepare($sql_last_message);
-                                    $requete_last_message->execute();
-                                    $last_message_result=$requete_last_message->fetch();
-                                    $last_message=$last_message_result['contenu'];
-                                };
+                                    $ami_pseudo=$ami['pseudo'];
+                                    $ami_id=$ami['id_ami'];
+                                    $sql_conv2= "SELECT * FROM conversations WHERE (utilisateur_1=$utilisateur OR utilisateur_2=$utilisateur) AND (utilisateur_1=$ami_id OR utilisateur_2=$ami_id)";
+                                    $requete_conv2=$connexion->prepare($sql_conv2);
+                                    $requete_conv2->execute();
+                                    $conv_result2=$requete_conv2->fetch();
+                                    if(!$conv_result2) {
+                                        $id_conv2="";
+                                        $last_message="Pas encore de message";
+                                    }
+                                    else {
+                                        $id_conv2=$conv_result2['id_conversation'];
+                                        $sql_last_message= "SELECT contenu FROM messages WHERE id_conversation=$id_conv2 ORDER BY time_stamp DESC LIMIT 1";
+                                        $requete_last_message=$connexion->prepare($sql_last_message);
+                                        $requete_last_message->execute();
+                                        $last_message_result=$requete_last_message->fetch();
+                                        $last_message=$last_message_result['contenu'];
+                                    };
                                 ?>
                                 <!-- Ici chaque ami à un lien vers une conversation entre lui et l'utilisateur connecté-->
                                 <div class="d-grid">
